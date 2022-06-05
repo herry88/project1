f912f8f6df4cb7947a3a9cde5a2fffec3f87b376
Herry Prasetyo <herry081288@gmail.com>
2022-06-05 14:31:40 +0700

    GetData

--- lib/helper/databasehelper.dart
+++ lib/helper/databasehelper.dart
@@ -17,10 +17,18 @@ class DBHelper {
     });
     status = response.body.contains('error');
     var res = json.decode(response.body);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       // savePref(res['data']['name'], res['user']['name']);
       print('data : ${res['error']}');
     }
+  }
 
+  //get data
+  Future<List> getData() async {
+    String myUrl = 'https://backendapilaravel-app.herokuapp.com/api/product';
+    http.Response response = await http.get(Uri.parse(myUrl), headers: {
+      'Accept': 'application/json',
+    });
+    return json.decode(response.body);
   }
 }
--- lib/screen/homePage.dart
+++ lib/screen/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? email;
+  getPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final value = prefs.getString('email') ?? 0;
+    setState(() {
+      print(value);
+      email = prefs.getString('email');
+      print(email);
+    });
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
